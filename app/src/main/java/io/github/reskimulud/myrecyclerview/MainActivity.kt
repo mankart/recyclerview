b55a4a3cd5a56c5edb868b36a441d5ecf59132b0
Reski Mulud Muchamad <reski.mulud@gmail.com>
2023-04-12 22:15:21 +0700

    my recyclerview

--- app/src/main/java/io/github/reskimulud/myrecyclerview/MainActivity.kt
+++ app/src/main/java/io/github/reskimulud/myrecyclerview/MainActivity.kt
@@ -2,10 +2,24 @@ package io.github.reskimulud.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.github.reskimulud.myrecyclerview.adapter.ListAlbumAdapter
+import io.github.reskimulud.myrecyclerview.data.AlbumData.listData
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listAdapter: ListAlbumAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listAdapter = ListAlbumAdapter()
+
+        val rvAlbums: RecyclerView = findViewById(R.id.rv_albums)
+        rvAlbums.layoutManager = LinearLayoutManager(this)
+        rvAlbums.adapter = listAdapter
+
+        listAdapter.setData(listData)
     }
 }
---
+++ app/src/main/java/io/github/reskimulud/myrecyclerview/adapter/ListAlbumAdapter.kt
@@ -0,0 +1,51 @@
+package io.github.reskimulud.myrecyclerview.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import io.github.reskimulud.myrecyclerview.R
+import io.github.reskimulud.myrecyclerview.model.Album
+
+class ListAlbumAdapter: RecyclerView.Adapter<ListAlbumAdapter.ViewHolder>() {
+    private val listAlbum: ArrayList<Album> = arrayListOf()
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val ivAlbumPicture: ImageView = itemView.findViewById(R.id.iv_album_picture)
+        private val tvTitle: TextView = itemView.findViewById(R.id.tv_title)
+        private val tvArtist: TextView = itemView.findViewById(R.id.tv_artist)
+        private val tvYear: TextView = itemView.findViewById(R.id.tv_year)
+
+        fun bind(data: Album) {
+            data.apply {
+                Glide.with(itemView.context)
+                    .load(cover)
+                    .into(ivAlbumPicture)
+                tvTitle.text = albumNames
+                tvArtist.text = albumArtist
+                tvYear.text = year
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listAlbum.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = listAlbum[position]
+        holder.bind(data)
+    }
+
+    fun setData(data: ArrayList<Album>) {
+        listAlbum.clear()
+        listAlbum.addAll(data)
+        notifyDataSetChanged()
+    }
+}
