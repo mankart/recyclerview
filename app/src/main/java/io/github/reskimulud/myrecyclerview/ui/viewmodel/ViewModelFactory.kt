c28164979cc24fa87000e7dc4aba31a71869ade8
Reski Mulud Muchamad <reski.mulud@gmail.com>
2023-05-14 08:04:13 +0700

    aac nonreg

---
+++ app/src/main/java/io/github/reskimulud/myrecyclerview/ui/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,17 @@
+package io.github.reskimulud.myrecyclerview.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import io.github.reskimulud.myrecyclerview.data.AlbumRepository
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory(private val repository: AlbumRepository): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel(repository) as T
+            }
+            else -> throw IllegalArgumentException("Model tidak ditemukan")
+        }
+    }
+}
